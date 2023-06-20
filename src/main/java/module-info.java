dc08cce9c558c7a25f534f1d4375cb2aec5113ec
Jean Lauth <lauth_jean@live.fr>
2023-06-20 14:02:25 +0200

    - mise en place MVC - configuration Maven + dépendances - création primaryStage + responsive

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.demo to javafx.fxml;
+    exports com.example.demo;
+    exports com.example.demo.controller;
+    opens com.example.demo.controller to javafx.fxml;
+}
